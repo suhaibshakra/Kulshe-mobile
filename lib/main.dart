c547aaa6cafe53ea66d2dd30397a58084cb44748
Suhaib Shakra <s.shakra@yahoo.com>
2021-05-19 14:40:10 +0300

    map after after "MAKETttttttttt"

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,63 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:kulshe/ui/ads_package/play_video.dart';
-import 'package:kulshe/ui/ads_package/public_ads_screen.dart';
-import 'package:kulshe/ui/ads_package/select_multi_img.dart';
-import 'package:kulshe/ui/ads_package/uplaod_images.dart';
-import 'package:kulshe/ui/auth/social_media/facebook.dart';
-import 'package:kulshe/ui/splash_screen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import './services_api/services.dart';
-import 'app_helpers/app_controller.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();//
-  SystemChrome.setPreferredOrientations([
-  DeviceOrientation.portraitUp,
-  DeviceOrientation.portraitDown
-  ]);
-  // SharedPreferences prefs = await SharedPreferences.getInstance();
-  // bool decision = prefs.getBool('x');
-  // Widget _screen =
-  // (decision == false || decision == null) ? PView() : LoginScreen();
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-  Future setLang() async {
-    SharedPreferences preferences = await SharedPreferences.getInstance();
-    preferences.setString("lang", 'ar');
-  }
-  @override
-  void initState() {
-    setState(() {
-      setLang().then((value){
-        SectionServicesNew.getSections();
-        CountriesServices.getCountries();
-      });
-    });
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: AppController.strings.appName,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MultiTest(),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:kulshe/ui/ads_package/play_video.dart';
+// import 'package:kulshe/ui/ads_package/public_ads_screen.dart';
+// import 'package:kulshe/ui/ads_package/select_multi_img.dart';
+// import 'package:kulshe/ui/ads_package/uplaod_images.dart';
+// import 'package:kulshe/ui/auth/social_media/facebook.dart';
+// import 'package:kulshe/ui/splash_screen.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+// import './services_api/services.dart';
+// import 'app_helpers/app_controller.dart';
+//
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();//
+//   SystemChrome.setPreferredOrientations([
+//   DeviceOrientation.portraitUp,
+//   DeviceOrientation.portraitDown
+//   ]);
+//   // SharedPreferences prefs = await SharedPreferences.getInstance();
+//   // bool decision = prefs.getBool('x');
+//   // Widget _screen =
+//   // (decision == false || decision == null) ? PView() : LoginScreen();
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//
+//   Future setLang() async {
+//     SharedPreferences preferences = await SharedPreferences.getInstance();
+//     preferences.setString("lang", 'ar');
+//   }
+//   @override
+//   void initState() {
+//     setState(() {
+//       setLang().then((value){
+//         SectionServicesNew.getSections();
+//         CountriesServices.getCountries();
+//       });
+//     });
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: AppController.strings.appName,
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primaryColor: Colors.blue,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: MultiTest(),
+//     );
+//   }
+// }
 
 // import 'dart:async';
 // import 'package:flutter/material.dart';
@@ -390,8 +390,14 @@ import 'package:flutter/material.dart';
 
 import 'app_helpers/app_colors.dart';
 
+import 'dart:async';
+
 
 void main() {
+  var timer = new Timer(const Duration(milliseconds: 10), () =>{ print("sss")});
+  timer.cancel();
+
+
   runApp(MyApp());
 }
 
